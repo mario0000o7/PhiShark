bb3eb533f8a9e59e61e91101e9ad581a874700c6
Tobiasz-Kali <tobiasz.kostrzewa@wp.pl>
2023-05-01 21:10:49 +0200

    Add campaign endpoints

---
+++ src/pages/api/l/[campaignId].ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import pool from '../../../components/mariadbPool'
+
+
+export default async function handler(req: NextApiRequest,res: NextApiResponse) {
+    const { campaignId } = req.query;
+    let decodedCampaignId = Buffer.from(campaignId as string, 'base64').toString('ascii');
+    let conn;
+    let result;
+    try {
+        conn = await pool.getConnection();
+        result = await conn.query("SELECT * FROM maile WHERE id_kampanii = ?", [id]);
+    } finally {
+        if (conn) conn.release(); //release to pool
+    }
+    res.status(200).json(result)
+}
+
+
