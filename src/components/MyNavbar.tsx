550fdace714b4b9b65eb8124351de03dd5b4ced8
pabeler <pabeler@o2.pl>
2023-06-03 14:20:09 +0200

    Delete duplicated button for forwarding to newCampaign

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -4,22 +4,22 @@ import Image from "next/image";
 import React from "react";
 export default function MyNavbar({title}:any) {
     return (
-            <Navbar  isBordered variant="static" maxWidth={'fluid'} css={{background:'#161C2E'}}>
-                <Navbar.Brand>
-                    <Image src={'/phisharklogo.png'} alt={'Logo'} width={70} height={70}/>
-                </Navbar.Brand>
+        <Navbar  isBordered variant="static" maxWidth={'fluid'} css={{background:'#161C2E'}}>
+            <Navbar.Brand>
+                <Image src={'/phisharklogo.png'} alt={'Logo'} width={70} height={70}/>
+            </Navbar.Brand>
 
-                <Navbar.Content>
-                    <Navbar.Item>
-                        <Link href="/newCampaign"><Button color="gradient" auto>Stwórz nową kampanię</Button></Link>
-                    </Navbar.Item>
-                    <Navbar.Item>
-                        <Link href="/CampaignDashboard"><Button color="gradient" auto>Lista kampanii</Button></Link>
-                    </Navbar.Item>
-                    <Text  size={50} style={{textAlign: 'center'}}>
-                        {title}
-                    </Text>
-                </Navbar.Content>
-            </Navbar>
+            <Navbar.Content>
+                <Navbar.Item>
+                    <Link href="/newCampaign"><Button color="gradient" auto>Stwórz nową kampanię</Button></Link>
+                </Navbar.Item>
+                <Navbar.Item>
+                    <Link href="/CampaignDashboard"><Button color="gradient" auto>Lista kampanii</Button></Link>
+                </Navbar.Item>
+                <Text  size={50} style={{textAlign: 'center'}}>
+                    {title}
+                </Text>
+            </Navbar.Content>
+        </Navbar>
     )
 }
